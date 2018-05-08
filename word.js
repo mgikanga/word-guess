217343eb1fdaedf48351f0a98afc1c04c7e389ee
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-05-07 17:31:06 -0700

    updated the word.js to split the word to guess

--- word.js
+++ word.js
@@ -1,5 +1,12 @@
 class Word{
-    constructor(){
-        
+    constructor(theWord){
+        this.theWord = theWord;
+    }
+    myLetter(){
+        var letter = this.theWord.split('');
+        console.log(letter)
+
     }
 }
+var newWord = new Word ("apple");
+newWord.myLetter();
